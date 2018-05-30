1142adbf7ddbdc499baf387181ca463f329f4847
dmitry106 <dimafeofanov@gmail.com>
2018-05-30 12:07:38 -0500

    Transitioning to cli application

--- program.py
+++ program.py
@@ -1,6 +1,13 @@
+import sys
+import os
 import csv
 import numpy
 from scipy.optimize import linear_sum_assignment
+from cement.core.foundation import CementApp
+from cement.core import hook
+from cement.core.controller import CementBaseController, expose
+from cement.utils.misc import init_defaults
+
 
 #Not needed for anything as of now
 class student:
@@ -22,7 +29,7 @@ class housingroom:
         self.room = room
         self.room_type = room_type
 
-costlist = []
+
 
 #cost for 
 roomtypecost = [5]
@@ -31,6 +38,8 @@ hallcost = [0,5,30,40] #NOTE: 2nd and 3rd cost needs to be high enough to offset
 wingcost = [0,5,10,15]
 wing2cost = [10]
 
+
+#NOTE: this function is for debugging purposes only
 def is_squared(matrix):
     # Check that all rows have the correct length, not just the first one
     return all(len(row) == len(matrix) for row in matrix)
@@ -77,70 +86,81 @@ def costofhousing (hall_friends_room,room_type,hall_choice,wing_1,wing_2,room_lo
     return score
 
 
+def main():
+    #get the arguments passed to the program
+    script = sys.argv[0]
+    action = sys.argv[1]
+    files = sys.argv[2:]
+    #only 1 function, to calculate the optimised housing list
+    assert action in ['--calc'], \
+           'Need action --calc ' + action
+    
 
-students = dict() #dict to iterate list of variables
-housingrooms = dict()
 
-maxnumberofstudent = 0
+    students = dict() #dict to iterate list of variables for the students
+    housingrooms = dict() #same for the rooms
 
-with open('MockHousingData.csv', newline='') as f1:
-    has_header = csv.Sniffer().has_header(f1.read(1024))
-    f1.seek(0)   #rewind to start
-    reader1 = csv.reader(f1)
-    if has_header:
-        next(reader1)
+    maxnumberofstudent = 0
 
-    for rowhousing in reader1:
-        if rowhousing[8] == "1":
+    costlist = []
+
+    with open(sys.argv[2], newline='') as f1:
+        has_header = csv.Sniffer().has_header(f1.read(1024)) #find that header and skip it
+        f1.seek(0)   #rewind to start
+        reader1 = csv.reader(f1)
+        if has_header:
             next(reader1)
-        maxnumberofstudent = maxnumberofstudent + 1 #NOTE: This is for later to determine the maximum number of loops to do so you don't end up iterating over the null values to make the matrix square
 
-        #just as with the class, the assigning of students to that class is unneeded as of now
-        students[int(rowhousing[0])] = student(rowhousing[0],rowhousing[1],rowhousing[2],rowhousing[3],rowhousing[4],rowhousing[5],rowhousing[6],rowhousing[7])
+        for rowhousing in reader1:
+            maxnumberofstudent = maxnumberofstudent + 1 #NOTE: This is for later to determine the maximum number of loops to do so you don't end up iterating over the null values to make the matrix square
+            #assigning the values of the student preferences
+            students[int(rowhousing[0])] = student(rowhousing[0],rowhousing[1],rowhousing[2],rowhousing[3],rowhousing[4],rowhousing[5],rowhousing[6],rowhousing[7])
 
-        rowlist = []
+            rowlist = []
+            
+            with open(sys.argv[3], newline='') as f2:
+                has_header=csv.Sniffer().has_header(f2.read(1024)) #skip the header
+                f2.seek(0)  #rewind to start
+                reader2 = csv.reader(f2)
+                if has_header:
+                    next(reader2)
+                
+                for rowroom in reader2:
+                    #Assign values for the room dict as well
+                    housingrooms[int(rowroom[0])] = housingroom(rowroom[0],rowroom[1],rowroom[2],rowroom[3],rowroom[4])
+                    #making the list that will be turned into a cost matrix
+                    rowlist.append(costofhousing(rowhousing[2],rowhousing[3],rowhousing[4],rowhousing[5],rowhousing[6],rowhousing[7],rowroom[1],rowroom[2],rowroom[3],rowroom[4]))
+            
+
+            costlist.append(rowlist)
         
-        with open('HallRoomData.csv', newline='') as f2:
-            has_header=csv.Sniffer().has_header(f2.read(1024))
-            f2.seek(0)  #rewind to start
-            reader2 = csv.reader(f2)
-            if has_header:
-                next(reader2)
-            for rowroom in reader2:
-                #print(rowhousing)  NOTE: debugging prints are left here for later should I have to mess with the dataset
-                #print(rowroom)
-                #print(costofhousing(rowhousing[2],rowhousing[3],rowhousing[4],rowhousing[5],rowhousing[6],rowhousing[7],rowroom[1],rowroom[2],rowroom[3],rowroom[4]))
-                housingrooms[int(rowroom[0])] = housingroom(rowroom[0],rowroom[1],rowroom[2],rowroom[3],rowroom[4])
-                rowlist.append(costofhousing(rowhousing[2],rowhousing[3],rowhousing[4],rowhousing[5],rowhousing[6],rowhousing[7],rowroom[1],rowroom[2],rowroom[3],rowroom[4]))
-                #print(rowlist)
-        costlist.append(rowlist)
-    
-#Convert the costlist into a numpy array for ease of manipulation
-costlist = numpy.asarray(costlist)
+    #Convert the costlist into a numpy array for ease of manipulation
+    costlist = numpy.asarray(costlist)
 
-#in order for the algorithm to optimise correctly it needs to be a square matrix
-costlist = pad_to_square(costlist, costlist.max())
+    #in order for the algorithm to optimise correctly it needs to be a square matrix
+    costlist = pad_to_square(costlist, costlist.max())
 
-#print(costlist)
-#print(is_squared(costlist)) NOTE: should result in affirmative in order for the rest to work
+    #print(is_squared(costlist)) NOTE: should result in affirmative in order for the rest to work
 
 
-#INSERT ACTUAL ALGORITHM HERE-------------------------------------------------------------------------!!!!!!!!!!!!!!!!!!!!
-resultmatrix = linear_sum_assignment(costlist)
-row_ind, col_ind = resultmatrix
-tuplesofhousing = list(zip(row_ind,col_ind))
-housingassignmentarray = numpy.asarray(tuplesofhousing)
-print(housingassignmentarray)
+    resultmatrix = linear_sum_assignment(costlist) #the magic happens here
+    row_ind, col_ind = resultmatrix
+    tuplesofhousing = list(zip(row_ind,col_ind))
+    housingassignmentarray = numpy.asarray(tuplesofhousing)
+    #print(housingassignmentarray)
 
-housingassignmentarray = numpy.asarray(list(map(lambda x: x+1 ,housingassignmentarray)))
+    housingassignmentarray = numpy.asarray(list(map(lambda x: x+1 ,housingassignmentarray)))
 
-with open("output.csv", 'w', newline='') as writer1:
-    writer = csv.writer(writer1)
-    
-    for row in housingassignmentarray:
-        if row[0] > maxnumberofstudent: #NOTE:NOTE:NOTE:NOTE THIS TOTAL TRASH IF STATEMENT IS A SAD SOLUTION FOR row[0] STUDENTS and I hate it
-            break
-        print(students[row[0]].name,"to",housingrooms[row[1]].hall,housingrooms[row[1]].wing,housingrooms[row[1]].room)
-        writer.writerow([students[row[0]].name,housingrooms[row[1]].hall,housingrooms[row[1]].wing,housingrooms[row[1]].room])
+    #Getting a csv ready to write the output to
+    with open(sys.argv[4], 'w', newline='') as writer1:
+        writer = csv.writer(writer1)
+        
+        for row in housingassignmentarray:
+            if row[0] > maxnumberofstudent: #NOTE:NOTE:NOTE:NOTE THIS TOTAL TRASH if STATEMENT IS A SAD SOLUTION FOR row[0] STUDENTS and I hate it
+                break
+            #print(students[row[0]].name,"to",housingrooms[row[1]].hall,housingrooms[row[1]].wing,housingrooms[row[1]].room)
+            writer.writerow([students[row[0]].name,housingrooms[row[1]].hall,housingrooms[row[1]].wing,housingrooms[row[1]].room])
 
-
+#bash/powershell convention
+if __name__ == '__main__':
+    main()
